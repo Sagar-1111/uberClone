27ef8363565d69a3350574f291d8a32e10cdb429
sagar <sagar.raval@raweng.com>
2017-09-21 23:20:29 +0530

    added find driver route and geo based query

--- controllers/drivers_controller.js
+++ controllers/drivers_controller.js
@@ -4,6 +4,15 @@ module.exports = {
   greeting(req,res){
     res.send({ hi: 'there'});
   },
+  index(req, res, next){
+    const { lng, lat } = req.query;
+    Driver.geoNear(
+      { type: 'Point', coordinates: [parseFloat(lng), parseFloat(lat)] },
+      { spherical:true, maxDistance: 200000 }
+    )
+      .then(drivers => res.send(drivers))
+      .catch(next);
+  },
   create(req, res, next){
     const driverProps = req.body;
     Driver.create(driverProps)
@@ -13,9 +22,14 @@ module.exports = {
   edit(req, res, next){
     const driverId = req.params.id;
     const driverProps = req.body;
-
     Driver.findByIdAndUpdate({ _id: driverId }, driverProps, { new: true })
       .then(driver => res.send(driver))
+      .catch(next);
+  },
+  delete(req, res, next){
+    const driverId =req.params.id;
+    Driver.findByIdAndRemove({ _id: driverId })
+      .then(driver => res.status(204).send(driver))
       .catch(next);
   }
 };
