30e83addd3537d00888846166e1909b915e0e4a0
sagar <sagar.raval@raweng.com>
2017-09-22 19:05:21 +0530

    added morgan to project

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const morgan = require('morgan');
 
 const routes = require('./routes/routes');
 const app = express();
@@ -8,6 +9,7 @@ const app = express();
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost/muber');
 
+app.use(morgan('dev'))
 app.use(bodyParser.json());
 routes(app);
 
